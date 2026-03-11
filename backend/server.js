840070fa93ef0ffa970d5fb3312400b475248174
sahishnuta <sahishnuta25@gmail.com>
2026-03-12 02:43:06 +0530

    Fix server.js: connect to Atlas, update login/signup HTML

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/codeBloom")
+ mongoose.connect(process.env.MONGO_URI)
+
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
